de3453c6144538094425f529d79e371221ee0628
jpvolponi <pierre.volponi@gmail.com>
2022-06-05 23:15:25 -0300

    updating

--- Exercism/go/gross-store/gross_store.go
+++ Exercism/go/gross-store/gross_store.go
@@ -1,5 +1,7 @@
 package gross
 
+import "fmt"
+
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
 	units := map[string]int{
@@ -10,7 +12,9 @@ func Units() map[string]int {
 		"gross":              144,
 		"great_gross":        1728,
 	}
+
 	return units
+
 }
 
 // NewBill creates a new bill.
@@ -21,19 +25,34 @@ func NewBill() map[string]int {
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	if _, ok := units[unit]; !ok {
+	if _, ok := units[unit]; !ok { //Return `false` if the given `unit` is not in the `units` map.
 		return ok
-	} else if _, ok := bill[item]; !ok {
-		return ok
-	} else {
+	} else if _, ok := bill[item]; !ok { //Otherwise add the item to the customer `bill`, indexed by the item name, then return `true`.
 		bill[item] = units[unit]
+		return true
+	} else { // If the item is already present in the bill, increase its quantity by the amount that belongs to the provided `unit`.D
+		bill[item] += units[unit]
 		return ok
 	}
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+	if _, ok := units[unit]; !ok {
+		return ok
+	} else if sera, ok := bill[item]; !ok {
+		return ok
+	} else if sera < 0 {
+		fmt.Println("Debug: ", sera)
+		return ok
+	} else if sera == 0 {
+		delete(bill, item)
+		fmt.Println("Debug: ", sera)
+		return true
+	} else {
+		bill[item] -= units[unit]
+		return true
+	}
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
