659f343ab6f16f702d39c90bea6c4e8c81964d0f
jpvolponi <pierre.volponi@gmail.com>
2022-06-08 00:09:08 -0300

    resolved

--- Exercism/go/gross-store/gross_store.go
+++ Exercism/go/gross-store/gross_store.go
@@ -1,7 +1,5 @@
 package gross
 
-import "fmt"
-
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
 	units := map[string]int{
@@ -38,18 +36,16 @@ func AddItem(bill, units map[string]int, item, unit string) bool {
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	if _, ok := units[unit]; !ok {
-		return ok
-	} else if sera, ok := bill[item]; !ok {
-		return ok
-	} else if sera < 0 {
-		fmt.Println("Debug: ", sera)
-		return ok
-	} else if sera == 0 {
+	if _, exists := bill[item]; !exists { //Return `false` if the given item is **not** in the bill
+		return exists
+	} else if _, exists := units[unit]; !exists { //Return `false` if the given `unit` is not in the `units` map.
+		return exists
+	} else if bill[item] < units[unit] { //Return `false` if the new quantity would be less than 0.
+		return false
+	} else if (bill[item] - units[unit]) == 0 { //If the new quantity is 0, completely remove the item from the `bill` then return `true`.
 		delete(bill, item)
-		fmt.Println("Debug: ", sera)
 		return true
-	} else {
+	} else { //Otherwise, reduce the quantity of the item and return `true`.
 		bill[item] -= units[unit]
 		return true
 	}
@@ -57,5 +53,7 @@ func RemoveItem(bill, units map[string]int, item, unit string) bool {
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+	value, exists := bill[item]
+	return value, exists
+
 }
