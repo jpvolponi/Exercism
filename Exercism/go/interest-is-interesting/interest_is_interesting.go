ac63422f61f6bfedf62c8ac95b3bef262777102d
jpvolponi <pierre.volponi@gmail.com>
2022-06-08 23:52:05 -0300

    updating

--- Exercism/go/interest-is-interesting/interest_is_interesting.go
+++ Exercism/go/interest-is-interesting/interest_is_interesting.go
@@ -2,7 +2,19 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	var rate float32
+	var blc float32 = float32(balance)
+	switch {
+	case blc < 0.0:
+		rate = 3.213
+	case blc >= 0.0 && blc < 1000.0:
+		rate = 0.5
+	case blc >= 1000.0 && blc < 5000.0:
+		rate = 1.621
+	default:
+		rate = 2.475
+	}
+	return rate
 }
 
 // Interest calculates the interest for the provided balance.
