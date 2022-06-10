c8ea1bcb6fd9d811be851691461b31d21b816e56
jpvolponi <pierre.volponi@gmail.com>
2022-06-10 00:08:27 -0300

    resolved

--- Exercism/go/interest-is-interesting/interest_is_interesting.go
+++ Exercism/go/interest-is-interesting/interest_is_interesting.go
@@ -19,15 +19,34 @@ func InterestRate(balance float64) float32 {
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	var interest_value float64
+
+	switch {
+	case balance < 0.0:
+		interest_value = balance * 0.03213
+	case balance >= 0.0 && balance < 1000.0:
+		interest_value = balance * 0.005
+	case balance >= 1000.0 && balance < 5000.0:
+		interest_value = balance * 0.01621
+	default:
+		interest_value = balance * 0.02475
+	}
+	return interest_value
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	var count int
+	var amount float64 = balance
+
+	for amount < targetBalance {
+		amount += Interest(amount)
+		count++
+	}
+	return count
 }
