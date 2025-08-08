6adeef7f11a87767a8c6a6323f277fa5997dc54a
jpvolponi <pierre.volponi@gmail.com>
2025-08-08 16:16:57 -0300

    Adiciona funções para cálculo de câmbio com spread e denominação

--- Exercism/python/currency-exchange/exchange.py
+++ Exercism/python/currency-exchange/exchange.py
@@ -14,6 +14,7 @@ def exchange_money(budget, exchange_rate):
     :param exchange_rate: float - unit value of the foreign currency.
     :return: float - exchanged value of the foreign currency you can receive.
     """
+    return budget / exchange_rate
 
     pass
 
@@ -25,7 +26,7 @@ def get_change(budget, exchanging_value):
     :param exchanging_value: float - amount of your money you want to exchange now.
     :return: float - amount left of your starting currency after exchanging.
     """
-
+    return budget - exchanging_value
     pass
 
 
@@ -36,7 +37,7 @@ def get_value_of_bills(denomination, number_of_bills):
     :param number_of_bills: int - total number of bills.
     :return: int - calculated value of the bills.
     """
-
+    return int((denomination/1) * number_of_bills)
     pass
 
 
@@ -47,7 +48,7 @@ def get_number_of_bills(amount, denomination):
     :param denomination: int - the value of a single bill.
     :return: int - number of bills that can be obtained from the amount.
     """
-
+    return int(amount // denomination)
     pass
 
 
@@ -58,6 +59,7 @@ def get_leftover_of_bills(amount, denomination):
     :param denomination: int - the value of a single bill.
     :return: float - the amount that is "leftover", given the current denomination.
     """
+    return amount % denomination
 
     pass
 
@@ -71,5 +73,16 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     :param denomination: int - the value of a single bill.
     :return: int - maximum value you can get.
     """
+    return int(exchange_money(budget, (exchange_rate * (1 + spread / 100))) // denomination) * denomination
 
     pass
+
+"""
+print(exchange_money(1.20, 1.2))
+print(get_change(127.5, 120))
+print(get_value_of_bills(5, 128))
+print(get_number_of_bills(127.5, 5))
+print(get_leftover_of_bills(127.5, 20))
+
+print(exchangeable_value(100000, 10.61, 10, 1))
+"""
